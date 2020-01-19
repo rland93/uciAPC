1946ba31d1b16e84c6da65c2f7629e77842bb7bf
Mike Sutherland <msutherl@uci.edu>
2020-01-19 14:39:19 -0800

    Create batch run script

---
+++ controller.py
@@ -0,0 +1,12 @@
+from simglucose.controller.base import Controller, Action
+
+class blankController(Controller):
+    def __init__(self, init_state):
+        self.init_state = init_state
+        self.state = init_state
+    def policy(self, observation, reward, done, **info):
+        self.state = observation
+        action = Action(basal=.03, bolus=0)
+        return action
+    def reset(self):
+        self.state = self.init_state
