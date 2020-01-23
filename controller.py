ded82a26c2c6d7fe59923e74c1bc6e75a1501945
rland93 <msutherland@fastmail.com>
2020-01-22 18:17:36 -0800

    create scripting api

--- controller.py
+++ controller.py
@@ -1,4 +1,12 @@
 from simglucose.controller.base import Controller, Action
+import pkg_resources
+import numpy as np
+import pandas as pd
+
+PATIENT_PARA_FILE = pkg_resources.resource_filename(
+    'simglucose', 'params/vpatient_params.csv')
+PATIENT_QUEST_FILE = pkg_resources.resource_filename(
+    'simglucose', 'params/Quest.csv')
 
 class blankController(Controller):
     def __init__(self, init_state):
@@ -10,3 +18,18 @@ class blankController(Controller):
         return action
     def reset(self):
         self.state = self.init_state
+
+class naivePI(Controller):
+    def __init__(self, init_state, patientID):
+        self.init_state = init_state
+        self.state = init_state
+        self.params = pd.read_csv(PATIENT_PARA_FILE)
+        self.quest = pd.read_csv(PATIENT_QUEST_FILE)
+        self.patientID = patientID
+        # total daily dose = patient weight in kg
+        self.tdd = self.params.loc[patientID,'BW']
+
+    def policy(self, observation, reward, done, **kwargs):
+        action = Action(basal=.2, bolus=0)
+        return action
+
